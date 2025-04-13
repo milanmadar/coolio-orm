813b6e181ea853a1ebd5b0a8baa512c56729c1be
Lot-Art <dev@lot-art.com>
2025-04-13 21:55:51 +0200

    docs

--- tests/Pgsql/fixtures/topology.sql
+++ tests/Pgsql/fixtures/topology.sql
@@ -1,5 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS postgis_topology;
+CREATE EXTENSION IF NOT EXISTS pgRouting;
 
 DO $$
 BEGIN
@@ -81,5 +82,47 @@ INSERT INTO public.topology_test (
      'topology_test_topo',                                -- topology schema
      4,                                              -- topology layer id 
      0.001                                           -- tolerance for snapping and validation
-)
+ )
+);
+
+-- Insert 'Second Street' connecting from (3,4) to (5,6)
+INSERT INTO public.topology_test (
+    name,
+    topo_geom_linestring
+) VALUES (
+ 'Second Street',
+toTopoGeom(
+     ST_GeomFromEWKT('SRID=4326;MULTILINESTRING((3 4, 5 6))'),
+     'topology_test_topo',
+     2,
+     0.001
+ )
+);
+
+-- Insert 'Third Avenue' connecting from (1,2) to (1,0)
+INSERT INTO public.topology_test (
+    name,
+    topo_geom_linestring
+) VALUES (
+ 'Third Avenue',
+toTopoGeom(
+     ST_GeomFromEWKT('SRID=4326;MULTILINESTRING((1 2, 1 0))'),
+     'topology_test_topo',
+     2,
+     0.001
+ )
+);
+
+-- Insert 'Fourth Boulevard' connecting from (5,6) to (7,6)
+INSERT INTO public.topology_test (
+    name,
+    topo_geom_linestring
+) VALUES (
+ 'Fourth Boulevard',
+toTopoGeom(
+     ST_GeomFromEWKT('SRID=4326;MULTILINESTRING((5 6, 7 6))'),
+     'topology_test_topo',
+     2,
+     0.001
+ )
 );
