5124e0677ef8c3775dac14bfa733c8d88be3ae70
milanmadar <milanmadar@hotmail.com>
2026-05-06 18:51:13 +0200

    geoUtil::getClosestPoint()

--- src/Geo/Utils.php
+++ src/Geo/Utils.php
@@ -2,6 +2,9 @@
 
 namespace Milanmadar\CoolioORM\Geo;
 
+use Doctrine\DBAL\Connection;
+use Milanmadar\CoolioORM\Manager;
+
 class Utils
 {
     /**
@@ -26,5 +29,183 @@ class Utils
         return (int)(
             ($lat >= 0) ? (32600 + $zone) : (32700 + $zone)
         );
+    }
+
+    /**
+     * @template T of AbstractShape
+     *
+     * @param T $geom
+     * @param int $targetSrid
+     * @param Manager|Connection $dbOrMgr
+     * @return T
+     */
+    public static function transformGeomToSrid(AbstractShape $geom, int $targetSrid, Manager|Connection $dbOrMgr): AbstractShape
+    {
+        $stTransform = GeoFunctions::ST_Transform($geom, $targetSrid);
+        $sql = "SELECT ST_AsEWKT(".$stTransform.")";
+
+        if($dbOrMgr instanceof Manager) {
+            $db = $dbOrMgr->getDb();
+        } else {
+            $db = $dbOrMgr;
+        }
+
+        $geomEWKT = $db->executeQuery($sql)->fetchOne();
+
+        /* @phpstan-ignore-next-line */
+        return Shape2D3D4DFactory::createFromGeoEWKTString($geomEWKT);
+    }
+
+    /**
+     * @param AbstractShape $geom1
+     * @param AbstractShape $geom2
+     * @param Manager|Connection $dbOrMgr
+     * @param int $roundToPrecision optional
+     * @return float
+     */
+    public static function getDistanceInMeters(AbstractShape $geom1, AbstractShape $geom2, Manager|Connection $dbOrMgr, int $roundToPrecision = -1): float
+    {
+        $stDistance = GeoFunctions::ST_Distance(
+            GeoFunctions::geography(
+                GeoFunctions::ST_Transform($geom1, 4326)
+            ),
+            GeoFunctions::geography(
+                GeoFunctions::ST_Transform($geom2, 4326)
+            )
+        );
+        $sql = "SELECT ".$stDistance;
+
+        if($dbOrMgr instanceof Manager) {
+            $db = $dbOrMgr->getDb();
+        } else {
+            $db = $dbOrMgr;
+        }
+
+        $v = (float)$db->executeQuery($sql)->fetchOne();
+        if($roundToPrecision > -1) {
+            return round($v, $roundToPrecision);
+        }
+        return $v;
+    }
+
+    /**
+     * @template T of Shape2D\Point|ShapeZ\PointZ|ShapeZM\PointZM
+     *
+     * @param AbstractShape $returnPointOnThisGeom
+     * @param T $closestToThisPoint
+     * @param Manager|Connection $dbOrMgr
+     * @return T
+     */
+    public static function getClosestPoint(AbstractShape $returnPointOnThisGeom, Shape2D\Point|ShapeZ\PointZ|ShapeZM\PointZM $closestToThisPoint, Manager|Connection $dbOrMgr): AbstractShape
+    {
+        // point, the closes point is itself
+        if($returnPointOnThisGeom instanceof Shape2D\Point
+        || $returnPointOnThisGeom instanceof ShapeZ\PointZ
+        || $returnPointOnThisGeom instanceof ShapeZM\PointZM)
+        {
+            return clone $returnPointOnThisGeom;
+        }
+        // multipooint
+        elseif($returnPointOnThisGeom instanceof Shape2D\MultiPoint
+        || $returnPointOnThisGeom instanceof ShapeZ\MultiPointZ
+        || $returnPointOnThisGeom instanceof ShapeZM\MultiPointZM)
+        {
+            $sql = "
+                SELECT ST_AsEWKT(
+                    ST_ClosestPoint(
+                        target,
+                        ST_Transform(p, ST_SRID(target))
+                    )
+                )
+                FROM (
+                    SELECT 
+                        ".GeoFunctions::ST_GeomFromEWKT_geom($returnPointOnThisGeom)." AS target,
+                        ".GeoFunctions::ST_GeomFromEWKT_geom($closestToThisPoint)." AS p
+                ) AS data;
+            ";
+        }
+        // line and multiline
+        elseif($returnPointOnThisGeom instanceof Shape2D\LineString
+        || $returnPointOnThisGeom instanceof ShapeZ\LineStringZ
+        || $returnPointOnThisGeom instanceof ShapeZM\LineStringZM
+        || $returnPointOnThisGeom instanceof Shape2D\MultiLineString
+        || $returnPointOnThisGeom instanceof ShapeZ\MultiLineStringZ
+        || $returnPointOnThisGeom instanceof ShapeZM\MultiLineStringZM)
+        {
+            $sql = "
+                SELECT ST_AsEWKT(
+                        ST_LineInterpolatePoint(
+                            line,
+                            ST_LineLocatePoint(line, ST_Transform(p, ST_SRID(line)))
+                        )
+                    )
+                FROM (
+                    SELECT 
+                        ".GeoFunctions::ST_GeomFromEWKT_geom($returnPointOnThisGeom)." AS line,
+                        ".GeoFunctions::ST_GeomFromEWKT_geom($closestToThisPoint)." AS p
+                ) AS data
+            ";
+        }
+        // curved geoms
+        elseif($returnPointOnThisGeom instanceof Shape2D\CircularString
+        || $returnPointOnThisGeom instanceof ShapeZ\CircularStringZ
+        || $returnPointOnThisGeom instanceof ShapeZM\CircularStringZM
+        || $returnPointOnThisGeom instanceof Shape2D\CompoundCurve
+        || $returnPointOnThisGeom instanceof ShapeZ\CompoundCurveZ
+        || $returnPointOnThisGeom instanceof ShapeZM\CompoundCurveZM
+        || $returnPointOnThisGeom instanceof Shape2D\CurvePolygon
+        || $returnPointOnThisGeom instanceof ShapeZ\CurvePolygonZ
+        || $returnPointOnThisGeom instanceof ShapeZM\CurvePolygonZM)
+        {
+            $sql = "
+                SELECT ST_AsEWKT(
+                        ST_LineInterpolatePoint(
+                            line,
+                            ST_LineLocatePoint(line, ST_Transform(p, ST_SRID(line)))
+                        )
+                    )
+                FROM (
+                    SELECT 
+                        ST_CurveToLine(".GeoFunctions::ST_GeomFromEWKT_geom($returnPointOnThisGeom).") AS line,
+                        ".GeoFunctions::ST_GeomFromEWKT_geom($closestToThisPoint)." AS p
+                ) AS data
+            ";
+        }
+        // polygon and multipolygon
+        elseif($returnPointOnThisGeom instanceof Shape2D\Polygon
+        || $returnPointOnThisGeom instanceof ShapeZ\PolygonZ
+        || $returnPointOnThisGeom instanceof ShapeZM\PolygonZM
+        || $returnPointOnThisGeom instanceof Shape2D\MultiPolygon
+        || $returnPointOnThisGeom instanceof ShapeZ\MultiPolygonZ
+        || $returnPointOnThisGeom instanceof ShapeZM\MultiPolygonZM)
+        {
+            $sql = "
+                SELECT ST_AsEWKT(
+                    ST_LineInterpolatePoint(
+                        boundary, 
+                        ST_LineLocatePoint(boundary, ST_Transform(p, ST_SRID(boundary)))
+                    )
+                )
+                FROM (
+                    SELECT 
+                        ST_Boundary(".GeoFunctions::ST_GeomFromEWKT_geom($returnPointOnThisGeom).") AS boundary,
+                        ".GeoFunctions::ST_GeomFromEWKT_geom($closestToThisPoint)." AS p
+                ) AS data;
+            ";
+        }
+        else {
+            throw new \InvalidArgumentException("Geo\Utils::getClosestPoint() doesnt support ".get_class($returnPointOnThisGeom));
+        }
+
+        if($dbOrMgr instanceof Manager) {
+            $db = $dbOrMgr->getDb();
+        } else {
+            $db = $dbOrMgr;
+        }
+
+        $geomEWKT = $db->executeQuery($sql)->fetchOne();
+
+        /* @phpstan-ignore-next-line */
+        return Shape2D3D4DFactory::createFromGeoEWKTString($geomEWKT);
     }
 }
---
+++ tests/Pgsql/GeoUtilsTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Pgsql;
+
+use PHPUnit\Framework\TestCase;
+use Milanmadar\CoolioORM\ORM;
+use Milanmadar\CoolioORM\Geo;
+
+class GeoUtilsTest extends TestCase
+{
+
+    // This method runs before every $this->test*() method runs
+    protected function setUp(): void
+    {
+        ORM::_clearSingleton();
+    }
+
+    public function testTransformGeomToSrid()
+    {
+        $db = \Milanmadar\CoolioORM\ORM::instance()->getDbByUrl($_ENV['DB_POSTGRES_DB1']);
+
+        $line = new Geo\ShapeZ\LineStringZ([
+            new Geo\ShapeZ\PointZ(1, 2, 3),
+            new Geo\ShapeZ\PointZ(4, 5, 6),
+            new Geo\ShapeZ\PointZ(7, 8, 9)
+        ], 4326);
+
+
+        $lineRegional = Geo\Utils::transformGeomToSrid($line, 32633, $db);
+        $this->assertInstanceOf(Geo\ShapeZ\LineStringZ::class, $lineRegional);
+        $this->assertEquals(32633, $lineRegional->getSRID());
+    }
+
+    public function testGetDistanceInMeters()
+    {
+        $db = \Milanmadar\CoolioORM\ORM::instance()->getDbByUrl($_ENV['DB_POSTGRES_DB1']);
+
+        $p1 = new Geo\Shape2D\Point(448250, 5302850, 32631);
+        $p2 = new Geo\Shape2D\Point(448350, 5302850, 32631);
+
+        $dist = Geo\Utils::getDistanceInMeters($p1, $p2, $db, 0);
+        $this->assertEquals(100, $dist);
+    }
+
+    public function testGetClosestPoint2D()
+    {
+        $db = \Milanmadar\CoolioORM\ORM::instance()->getDbByUrl($_ENV['DB_POSTGRES_DB1']);
+
+        $srid = 32631;
+        $line = new Geo\Shape2D\LineString([
+            new Geo\Shape2D\Point(0, 0, $srid),
+            new Geo\Shape2D\Point(100, 100, $srid),
+        ]);
+        $point = new Geo\Shape2D\Point(0, 100, $srid);
+
+        $snappedPoint = Geo\Utils::getClosestPoint($line, $point, $db);
+        $this->assertEquals(50, (int)$snappedPoint->getX());
+        $this->assertEquals(50, (int)$snappedPoint->gety());
+        $this->assertEquals($srid, $snappedPoint->getSRID());
+    }
+
+    public function testGetClosestPointZM()
+    {
+        $db = \Milanmadar\CoolioORM\ORM::instance()->getDbByUrl($_ENV['DB_POSTGRES_DB1']);
+
+        $srid = 32631;
+        $line = new Geo\ShapeZM\LineStringZM([
+            new Geo\ShapeZM\PointZM(0, 0, 0, 1111, $srid),
+            new Geo\ShapeZM\PointZM(100, 100, 100, 2222, $srid),
+        ]);
+        $point = new Geo\ShapeZM\PointZM(0, 100, 100, 3333, $srid);
+
+        $snappedPoint = Geo\Utils::getClosestPoint($line, $point, $db);
+        $this->assertEquals(50, (int)$snappedPoint->getX());
+        $this->assertEquals(50, (int)$snappedPoint->getY());
+        $this->assertEquals(50, (int)$snappedPoint->getZ());
+        $this->assertEquals($srid, $snappedPoint->getSRID());
+    }
+}
